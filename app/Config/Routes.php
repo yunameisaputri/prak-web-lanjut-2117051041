e5032be0fbfa1f454262ddaf2b826e7bda4194c4
Yuna Meisa Putri <yunameisayunameisa@gmail.com>
2023-09-22 07:47:09 +0700

    Pertemuan ke 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,17 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
-
+use App\Controllers\UserController;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+$routes->get('/', 'Home::index');
+$routes->get('/user/profile/(:any)/(:any)', 'UserController::profile/$1/$2');
+
+#form
+$routes->get ('/user/profile', [UserController::class, 'profile']);
+$routes->get ('/user/create', [UserController::class, 'create']);
+$routes->post ('/user/store', [UserController::class, 'store']);
+
+
 
